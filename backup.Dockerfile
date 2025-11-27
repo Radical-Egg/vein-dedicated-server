f25005926ee4c18a64e87bdd425efc5607f86ee3
egg <egg95@protonmail.com>
2025-11-26 18:42:20 -0800

    fix to run backup as PUID

--- backup.Dockerfile
+++ backup.Dockerfile
@@ -1,9 +1,12 @@
 FROM alpine:latest
 
-RUN apk update && \
-    apk add rsync bash findutils coreutils
-
 ENV VEIN_SERVER_BACKUP_INTERVAL_SECONDS=3600
+
+RUN apk update && \
+    apk add rsync bash findutils coreutils gosu shadow
+
+RUN addgroup -g 1000 vein \
+    && adduser -u 1000 -G vein -s /bin/bash -D vein
 
 COPY ./bin/backup_game_data.sh /usr/local/bin/backup_game_data
 RUN chmod +x usr/local/bin/backup_game_data
--- bin/backup_game_data.sh
+++ bin/backup_game_data.sh
@@ -2,10 +2,18 @@
 
 set -euo pipefail
 
+PGID="${PGID:-1000}"
+PUID="${PUID:-1000}"
+VEIN_USER="vein"
+VEIN_GROUP="vein"
 VEIN_SERVER_BACKUP_SRC_DIR=${VEIN_SERVER_BACKUP_SRC_DIR:-/data}
 VEIN_SERVER_BACKUP_DIR=${VEIN_SERVER_BACKUP_DIR:-/backup}
 VEIN_SERVER_BACKUP_RETENTION=${VEIN_SERVER_BACKUP_RETENTION:-5}
 INTERVAL="${VEIN_SERVER_BACKUP_INTERVAL_SECONDS:-3600}"
+
+export PGID PUID VEIN_USER VEIN_GROUP \
+       VEIN_SERVER_BACKUP_SRC_DIR VEIN_SERVER_BACKUP_DIR \
+       VEIN_SERVER_BACKUP_RETENTION INTERVAL
 
 main() {
     now="$(date +%Y-%m-%d_%H%M%S).backup"
@@ -30,7 +38,20 @@ main() {
 }
 
 while true; do
-    main
+    if [ "$(id -u "${VEIN_USER}")" != "${PUID}" ]; then
+        usermod -o -u "${PUID}" "${VEIN_USER}"
+    fi
+
+    if [ "$(getent group "${VEIN_GROUP}" | cut -d: -f3)" != "${PGID}" ]; then
+        groupmod -o -g "${PGID}" "${VEIN_GROUP}"
+    fi
+
+    if [ "$(stat -c %u "${VEIN_SERVER_BACKUP_DIR}")" != "${PUID}" ]; then
+        echo "Adjusting permissions for ${VEIN_SERVER_BACKUP_DIR}.."
+        chown -R "${VEIN_USER}:${VEIN_GROUP}" "${VEIN_SERVER_BACKUP_DIR}"
+    fi
+
+    gosu "${VEIN_USER}" bash -c "$(declare -f main); main"
     echo "[backup] Sleeping for $INTERVAL"
     sleep "${INTERVAL}"
 done
